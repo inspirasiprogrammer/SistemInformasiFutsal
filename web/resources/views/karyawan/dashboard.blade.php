c99f8bde03e4de8ffe09a4bf694650b7a4c3f1a1
unknown <fahmiaffanh>
2019-10-29 06:26:40 +0700

    update 291019

--- web/resources/views/karyawan/dashboard.blade.php
+++ web/resources/views/karyawan/dashboard.blade.php
@@ -3,6 +3,7 @@
 @section('title','Home')
 
 @section('content')
+    <div style="margin-bottom: 50px"></div>
     <div class="row">
         <div class="col-md-6 col-sm-12 col-xs-12">
             <div class="card box-shadow" style="margin:20px 50px; ">
@@ -15,12 +16,12 @@
             </div>          
         </div>
         <div class="col-md-6 col-sm-12 col-xs-12">
-            <div class="card box-shadow" style="margin:20px 50px; ">
+            <div class="card box-shadow" style="margin:60px 50px; ">
                 <a href="" class="btn btn-success btn-block" style="line-height:60px; height:80px; font-size:20px;">Customer</a>
             </div>          
         </div>
         <div class="col-md-6 col-sm-12 col-xs-12">
-            <div class="card box-shadow" style="margin:20px 50px; ">
+            <div class="card box-shadow" style="margin:60px 50px; ">
                 <a href="" class="btn btn-success btn-block" style="line-height:60px; height:80px; font-size:20px;">Lapangan</a>
             </div>          
         </div>
---
+++ web/resources/views/transaksi/dashboard.blade.php
@@ -0,0 +1,20 @@
+@extends('karyawan.main')
+
+@section('title','Home')
+
+@section('content')
+    <div style="margin-bottom: 200px"></div>
+    <div class="row">
+        <div class="col-md-6 col-sm-12 col-xs-12">
+            <div class="card box-shadow" style="margin:20px 50px; ">
+                <a href="" class="btn btn-success btn-block" style="line-height:60px; height:80px; font-size:20px;">Penjualan</a>
+            </div>          
+        </div>
+        <div class="col-md-6 col-sm-12 col-xs-12">
+            <div class="card box-shadow" style="margin:20px 50px; ">
+                <a href="{{route('item.list')}}" class="btn btn-success btn-block" style="line-height:60px; height:80px; font-size:20px;">Pembelian</a>
+            </div>          
+        </div>
+        
+    </div>
+@endsection
