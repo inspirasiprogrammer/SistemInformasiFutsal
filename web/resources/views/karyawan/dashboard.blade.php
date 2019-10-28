a914284d2b41fafd092c35a6e646f1c4cb8e444f
unknown <fahmiaffanh>
2019-10-28 12:04:41 +0700

    desain dashboard

--- web/resources/views/karyawan/dashboard.blade.php
+++ web/resources/views/karyawan/dashboard.blade.php
@@ -5,24 +5,25 @@
 @section('content')
     <div class="row">
         <div class="col-md-6 col-sm-12 col-xs-12">
-            <div class="card box-shadow">
-                <img src="" alt="thumbnail" class="card-img-top">
+            <div class="card box-shadow" style="margin:20px 50px; ">
+                <a href="" class="btn btn-success btn-block" style="line-height:60px; height:80px; font-size:20px;">Transaksi</a>
             </div>          
         </div>
         <div class="col-md-6 col-sm-12 col-xs-12">
-            <div class="card box-shadow">
-                <a href="" class="btn btn-success btn-block">Penjualan</a>
+            <div class="card box-shadow" style="margin:20px 50px; ">
+                <a href="" class="btn btn-success btn-block" style="line-height:60px; height:80px; font-size:20px;">Item</a>
             </div>          
         </div>
         <div class="col-md-6 col-sm-12 col-xs-12">
-            <div class="card box-shadow">
-                <a href="" class="btn btn-success btn-block">Penjualan</a>
+            <div class="card box-shadow" style="margin:20px 50px; ">
+                <a href="" class="btn btn-success btn-block" style="line-height:60px; height:80px; font-size:20px;">Customer</a>
             </div>          
         </div>
         <div class="col-md-6 col-sm-12 col-xs-12">
-            <div class="card box-shadow">
-                <a href="" class="btn btn-success btn-block">Penjualan</a>
+            <div class="card box-shadow" style="margin:20px 50px; ">
+                <a href="" class="btn btn-success btn-block" style="line-height:60px; height:80px; font-size:20px;">Lapangan</a>
             </div>          
         </div>
+        
     </div>
 @endsection
