efaa3710cfe4f3a280de986b3b9fa0e28517e32c
fahmiaffanh <fahmiaffanh@protonmail.com>
2019-12-03 10:20:22 +0700

    Javascript tombol halaman bayar

--- web/resources/views/main.blade.php
+++ web/resources/views/main.blade.php
@@ -29,6 +29,8 @@
     <link rel="stylesheet" href="{{ asset("plugins/summernote/summernote-bs4.css") }}">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+    @yield('script')
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
--- web/resources/views/penjualan/bayar.blade.php
+++ web/resources/views/penjualan/bayar.blade.php
@@ -28,54 +28,54 @@
                 </div>
                 <div class="col-md-6 col-sm-12 col-xs-12">
                     <label for="kembalian">Kembalian</label>
-                    <input type="number" class="form-control " name="kembalian" id="kembalian" style="width:50%; pointer-events: none;" value="30000">
+                    <input type="number" class="form-control " name="kembalian" id="kembalian" style="width:50%; pointer-events: none;">
                 </div>
                 <div class="col-md-6 col-sm-12 col-xs-12"  style="margin-bottom:50px;">
                     <label for="bayar">Bayar</label>
-                    <input type="text" class="form-control " name="bayar" id="bayar" style="width:50%;" >
+                    <input type="text" class="form-control " name="bayar" id="bayar" style="width:50%;" value='0'>
                 </div>
                 <div class="col-md-6 col-sm-12 col-xs-12"  style="margin-bottom:50px;">
                     
                 </div>
                 <div class="col-md-3" style="margin-bottom:20px;">
                     <div class="card box-shadow">
-                        <a href="" class="btn btn-primary btn-block">100000</a>
+                        <button class="btn btn-primary btn-block" onClick="testing(100000)">100000</button>
                     </div>          
                 </div>
                 <div class="col-md-3" style="margin-bottom:20px;">
                     <div class="card box-shadow">
-                        <a href="" class="btn btn-primary btn-block">20000</a>
+                        <button class="btn btn-primary btn-block" onClick="testing(20000)">20000</button>
                     </div>          
                 </div>
                 <div class="col-md-3" style="margin-bottom:20px;">
                     <div class="card box-shadow">
-                        <a href="" class="btn btn-primary btn-block" onclick="testing()">5000</a>
+                        <button class="btn btn-primary btn-block" onClick="testing(5000)">5000</button>
                     </div>          
                 </div>
                 <div class="col-md-3" style="margin-bottom:20px;">
                     <div class="card box-shadow">
-                        <a href="" class="btn btn-primary btn-block">1000</a>
+                        <button class="btn btn-primary btn-block" onClick="testing(1000)">1000</button>
                     </div>          
                 </div>
         
                 <div class="col-md-3">
                     <div class="card box-shadow">
-                        <a href="" class="btn btn-primary btn-block">50000</a>
+                        <button class="btn btn-primary btn-block" onClick="testing(50000)">50000</button>
                     </div>          
                 </div>
                 <div class="col-md-3">
                     <div class="card box-shadow">
-                        <a href="" class="btn btn-primary btn-block">10000</a>
+                        <button class="btn btn-primary btn-block" onClick="testing(10000)">10000</button>
                     </div>          
                 </div>
                 <div class="col-md-3">
                     <div class="card box-shadow">
-                        <a href="" class="btn btn-primary btn-block">2000</a>
+                        <button class="btn btn-primary btn-block" onClick="testing(2000)">2000</button>
                     </div>          
                 </div>
                 <div class="col-md-3">
                     <div class="card box-shadow">
-                        <a href="" class="btn btn-primary btn-block">500</a>
+                        <button class="btn btn-primary btn-block" onClick="testing(500)">500</button>
                     </div>          
                 </div>
         
@@ -83,7 +83,8 @@
                     <input type="submit" class="btn btn-success" name="simpan" style="margin-top:35px;" value="Simpan">
                 </div>
                 <div class="col-md-6 col-sm-12 col-xs-12"  style="">
-                <input type="submit" class="btn btn-success" name="ulang" style="margin-top:35px;" value="Ulang">
+                {{-- <input type="submit" class="btn btn-success" name="ulang" style="margin-top:35px;" value="Ulang"> --}}
+                <button class="btn btn-success" onClick="ulang()" style="margin-top:35px;">Ulang</button>
                 </div>
                 
             
@@ -97,7 +98,13 @@
 @endsection
 @section('script')
  <script>
- function testing() {
-        document.getElementById("bayar").innerHTML='5000';
-      }</script>
+ function testing(nilai) {
+        document.getElementById("bayar").value=Number(document.getElementById("bayar").value)+Number(nilai);
+        document.getElementById("kembalian").value=Number(document.getElementById("bayar").value)-Number(document.getElementById("total").value);
+      }
+    function ulang(){
+        document.getElementById("bayar").value=0;
+        document.getElementById("kembalian").value=0
+    }
+      </script>
 @endsection
